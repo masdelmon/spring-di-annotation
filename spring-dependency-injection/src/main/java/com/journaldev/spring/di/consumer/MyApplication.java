4008d6f2e9e26149bb0a31a0d7acda4b2be6f8d0
Massimiliano <masdelmon@hotmail.it>
2019-03-02 18:35:26 +0100

    Update MyApplication.java

--- spring-dependency-injection/src/main/java/com/journaldev/spring/di/consumer/MyApplication.java
+++ spring-dependency-injection/src/main/java/com/journaldev/spring/di/consumer/MyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
-import com.journaldev.spring.di.services.MessageService;
+//import com.journaldev.spring.di.services.MessageService;
 
 @Component
 public class MyApplication {
